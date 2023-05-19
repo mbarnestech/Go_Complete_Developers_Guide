0dd229e2b4bcaf4abf83c2e5e70c360bdcd9469d
mbarnestech <maggie.barnes@gmail.com>
2023-05-19 14:32:10 -0600

    add shapes, maps, and interfaces

--- maps/main.go
+++ maps/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	colors := map[string]string{
+		"red":   "#FF0000",
+		"green": "#00FF00",
+	}
+	printMap(colors)
+}
+
+func printMap(m map[string]string) {
+	for color, hex := range m {
+		fmt.Println(color, hex)
+	}
 }
